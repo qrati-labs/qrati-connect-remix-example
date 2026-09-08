07022f6f19a6503194d669f56275ba7c190e95cd
Siddhartha Maity <siddharthamaity1993@gmail.com>
2026-09-08 14:27:48 +0530

    feat: publish production Remix example

---
+++ app/qrati-connect.d.ts
@@ -0,0 +1,9 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    'qrati-connect': {
+      'organization-id'?: string
+      theme?: 'light' | 'dark'
+      router?: 'hash' | 'memory'
+    }
+  }
+}
